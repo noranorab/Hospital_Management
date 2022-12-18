7ed7d1a8c6c13ce967d60e4d2d405a30b199db71
noranorab <117985303+noranorab@users.noreply.github.com>
2022-12-18 12:43:07 +0100

    Update Consultation.java

--- Consultation.java
+++ Consultation.java
@@ -1,115 +1,193 @@
 package Gestion;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
-public class Medecin {
-	private int matricule;
-	private String nom;
-	private String contact;
-	private Specialite specialite;
-	private List<Patient> patients = new ArrayList<Patient>();
-	private List<Consultation> consultation = new ArrayList<Consultation>();
-	//private
-	
-	
-	Medecin(){}
-	Medecin(int matricule, String nom, String contact,Boolean estDispo, Specialite specialite){
-		this.matricule = matricule;
-		this.nom = nom;
-		this.contact = contact;
-		this.specialite = specialite;
-		
-	}
-	//setters & getters (penser à exception pour matricule)
-	public void setMatricule(int matricule) {
-		this.matricule = matricule;
-	}
-	
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	
-	public void setContact(String contact) {
-		this.contact = contact;
+public class Consultation1 implements Comparable{
+
+    private int matricule = 0;
+    private Patient patient;
+    private Medecin medecin;
+    private Date date;
+    private List<Consultation> listConsultation = new ArrayList();
+    private List listMedecin = new ArrayList<Medecin>();
+    
+    public Consultation() {}
+
+    // exception si une consultation est faite à la meme heure
+	private Consultation(Date date, Medecin medecin, Patient patient){
+		this.matricule = matricule++;
+		this.date = date;
+		this.patient = patient;
+		this.medecin = medecin;
 	}
 	
 	public int getMatricule() {
-		return this.matricule;
-	}
-	
-	public String getNom() {
-		return this.nom;
-	}
-	
-	public String getContact() {
-		return this.contact;
+		return matricule;
 	}
 
-	public Specialite getSpecialite() {
-		return specialite;
+	public void setMatricule(int matricule) {
+		this.matricule = matricule;
 	}
-	public void setSpecialite(Specialite specialite) {
-		this.specialite = specialite;
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Medecin getMedecin() {
+		return medecin;
+	}
+
+	public void setMedecin(Medecin medecin) {
+		this.medecin = medecin;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		try {
+			if (!(this.dateIsValid(date))) {
+				int min = date.getMinutes() + 30;
+				date.setMinutes(min);
+				this.date = date;
+			}
+		}catch(Exception e) {
+			consultationException consul = new consultationException();
+			System.out.println(consul);
+		}
+		this.date = date;
+		
+	}
+	public Boolean dateIsValid(Date date) {
+		for (Consultation1 consul : this.listConsultation) {
+			if (consul.compareTo(this) == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
-	public Patient rechercherPatient(int matricule) {
-		int a = 0;
-		for (Patient m: this.patients) {
-			if (m.getMatricule() == matricule) {
-				return m;
+	public Medecin rechercherMedecinParSpecialite(Specialite specialite) {
+		for (Object medecin : this.listMedecin) {
+			if (((Medecin) medecin).getSpecialite() == specialite) {
+				return (Medecin) medecin;
 			}
 		}
 		return null;
 	}
 	
-	public Consultation rechercherConsultation(Consultation consul) {
-		for (Consultation consultation: this.consultation) {
-			if (consultation.compareTo(consul) == 0) {
-				return consultation;
+	//affecter patient au medecin selon symptomes et planning
+	public void programmerConsultation(Patient patient) throws consultationException{
+		Medecin medecin = new Medecin();
+		Date date = new Date();
+		Consultation consultation = new Consultation();
+		if (patient.getSymptomes() == Symptomes.DIABETE) {
+			medecin = rechercherMedecinParSpecialite(Specialite.DIABETOLOGUE);
+			consultation.setMedecin(medecin);
+			consultation.setDate(date);
+			
+			consultation.setPatient(patient);
+			if (medecin.rechercherConsultation(consultation) != null) {
+				medecin.ajouterConsultation(consultation);
+			}else{
+				Date newDate = new Date();
+				int min = newDate.getMinutes() + 30;
+				newDate.setMinutes(min);
+				consultation.setDate(date);//exception!!
+			}	
+		}
+		if (patient.getSymptomes() == Symptomes.PROBLEMES_ARTICULATIONS) {
+			medecin = rechercherMedecinParSpecialite(Specialite.RHUMATOLOGUE);
+			consultation.setMedecin(medecin);
+			consultation.setDate(date);
+			consultation.setPatient(patient);
+			if (medecin.rechercherConsultation(consultation) != null) {
+				medecin.ajouterConsultation(consultation);
+			}else{
+				Date newDate = new Date();
+				int min = newDate.getMinutes() + 30;
+				newDate.setMinutes(min);
+				consultation.setDate(date);//exception!!
 			}
 		}
-		return null;
+		if (patient.getSymptomes() == Symptomes.PROBLEMES_VENTRES) {
+			medecin = rechercherMedecinParSpecialite(Specialite.GYNECOLOGUE);
+			consultation.setMedecin(medecin);
+			consultation.setDate(date);
+			consultation.setPatient(patient);
+			if (medecin.rechercherConsultation(consultation) != null) {
+				medecin.ajouterConsultation(consultation);
+			}else{
+				Date newDate = new Date();
+				int min = newDate.getMinutes() + 30;
+				newDate.setMinutes(min);
+				consultation.setDate(date);//exception!!
+				}
+			}
+		if (patient.getSymptomes() == Symptomes.PROBLEMES_COEUR) {
+			medecin = rechercherMedecinParSpecialite(Specialite.CARDIOLOGUE);
+			consultation.setMedecin(medecin);
+			consultation.setDate(date);
+			consultation.setPatient(patient);
+			if (medecin.rechercherConsultation(consultation) != null) {
+				medecin.ajouterConsultation(consultation);
+			}else{
+				Date newDate = new Date();
+				int min = newDate.getMinutes() + 30;
+				newDate.setMinutes(min);
+				consultation.setDate(date);//exception!!
+			}
+		}	
+		
 	}
 	
-	//permet de voir informations sur patient par son matricule
-	public void consulterPatient(int matricule) {
-		System.out.println("Nom du patient : " + rechercherPatient(matricule).getNom() + '\n' +
-		                   "Matricule du patient : " + rechercherPatient(matricule).getMatricule()
-		                   );
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, matricule);
 	}
-	//toString
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Consultation other = (Consultation) obj;
+		return Objects.equals(date, other.date) && matricule == other.matricule;
+	}
+
 	@Override
 	public String toString() {
-		return "Médecin : " + this.matricule + ", " + this.nom + ", "
-				+ this.contact;
-	}
-	
-	//methode pour affecter medicin à patient selon maladie patient
-	public void ajouterPatient(Patient patient) {
-		if (!(patients.contains(patient))) {
-			patients.add(patient);
-		}else {
-			System.out.println("patient existe");
-		}
-		
-	}
-	public void ajouterConsultation(Consultation consul) {
-		if (!(this.consultation.contains(consul))) {
-			this.consultation.add(consul);
-		}else {
-			System.out.println("consultation déjà programmée");
-		}
 		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
-	
-	
-	
-	
+		return "numero de consultation : "+this.matricule+ '\n'
+				+ "à " + this.date +'\n'+ "medecin : " + this.medecin.getNom() +
+				'\n' + "patient : " + this.patient.getNom();
+		}
 
-}
+	@Override 
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		if (date == ((Consultation) o).getDate() && getMedecin() == ((Consultation) o).getMedecin()) {
+			return 0;
+		}else {
+			if (date.compareTo(((Consultation) o).getDate()) > 0){
+				return 1;	
+			}else {
+				return -1;
+			}
+		}
+	}
+
+
